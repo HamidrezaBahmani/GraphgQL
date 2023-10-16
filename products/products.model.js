4f3b302aa01e0fa34f1e919d2d31742e77833e0d
HamidrezaBahmani <hb.hamidrezabahmani.com@gmail.com>
2023-10-16 13:39:14 +0330

    GraphQL example

---
+++ products/products.model.js
@@ -0,0 +1,61 @@
+const products = [
+  {
+    id: "redshoe",
+    description: "Red Shoe",
+    price: 42.12,
+    reviews: [],
+  },
+  {
+    id: "bluejean",
+    description: "Blue Jeans",
+    price: 55.55,
+    reviews: [],
+  },
+];
+
+function getAllProducts() {
+  return products;
+}
+
+function getProductsByPrice(min, max) {
+  return products.filter((product) => {
+    return product.price >= min && product.price <= max;
+  });
+}
+
+function getProductsById(id) {
+  return products.find((product) => {
+    return product.id === id;
+  });
+}
+
+function addNewProduct(id, description, price) {
+  const newProduct = {
+    id,
+    price,
+    description,
+    reviews: [],
+  };
+  products.push(newProduct);
+  return newProduct;
+}
+
+function addNewProductReview(id, rating, comment) {
+  const matchedProduct = getProductsById(id);
+  if (matchedProduct) {
+    const newProductReview = {
+      rating,
+      comment,
+    };
+    matchedProduct.reviews.push(newProductReview);
+    return newProductReview;
+  }
+}
+
+module.exports = {
+  getAllProducts,
+  getProductsByPrice,
+  getProductsById,
+  addNewProduct,
+  addNewProductReview,
+};
---
+++ server.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
+
+const { loadFilesSync } = require("@graphql-tools/load-files");
+const { makeExecutableSchema } = require("@graphql-tools/schema");
+
+const typesArray = loadFilesSync(path.join(__dirname, "**/*.graphql"));
+const resolversArray = loadFilesSync(path.join(__dirname, "**/*.resolvers.js"));
+
+async function startApolloServer() {
+  const app = express();
+  const schema = makeExecutableSchema({
+    typeDefs: typesArray,
+    resolvers: resolversArray,
+  });
+
+  const server = new ApolloServer({
+    schema,
+  });
+  await server.start();
+  server.applyMiddleware({ app, path: "/graphql" });
+
+  app.listen(3000, () => {
+    console.log(`Runnung GraphQL server...`);
+  });
+}
+
+startApolloServer();
